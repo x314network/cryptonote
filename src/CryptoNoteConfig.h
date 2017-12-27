6b4a7510f503105bac9f73e69bed7c5c15fa8bc8
x314.network <hello@x314.network>
2017-12-27 04:27:26 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,6 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "52.170.210.226:31477",
+  "52.166.109.99:21477",
 };
 
 struct CheckpointData {
